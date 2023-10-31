4bd990c2a53446678be289531b51916d95bea5cc
ItsMeMichaeluh <113515104+ItsMeMichaeluh@users.noreply.github.com>
2023-10-31 12:35:50 +0100

    update grotere resoultie

--- index/index.js
+++ index/index.js
@@ -1,7 +1,13 @@
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
 });
+
+const hiddenElements = document.querySelectorAll('hidden');
+hiddenElements.foreach((el) => observer.observer(el));
